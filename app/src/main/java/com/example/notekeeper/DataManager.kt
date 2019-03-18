7082acc327bfac01ae6c911c201cd3237cbd6ce5
peibol71 <peibol@rocketmail.com>
2019-03-18 13:41:13 +0100

    Modulo 5 y medio New activity, icon, etc.

--- app/src/main/java/com/example/notekeeper/DataManager.kt
+++ app/src/main/java/com/example/notekeeper/DataManager.kt
@@ -1,8 +1,26 @@
 package com.example.notekeeper
 
 import android.provider.ContactsContract
+import android.telephony.mbms.MbmsErrors
 
 class DataManager {
     var courses = HashMap<String, CourseInfo>()
     val notes = ArrayList<NoteInfo>()
+
+    init {
+        initializeCourses()
+    }
+    private fun initializeCourses() {
+        var course = CourseInfo("android_intents", "Android Programming with Intents")
+        courses.set(course.courseId, course)
+
+        course= CourseInfo("android_async","Android Async Programming and Services")
+        courses.set(course.courseId,course)
+
+        course = CourseInfo(title = "Java Fundamentals: The Java Language", courseId = "java_lang")
+        courses.set(course.courseId, course)
+
+        course = CourseInfo("java_core", "Java Fundamentals: The Core Platform")
+        courses.set(course.courseId, course)
+    }
 }
